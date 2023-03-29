9d2d356c63e948b469d73e1b71c5aa77073f33f6
Pelegrino <pelegrino@gmail.com>
2023-03-29 14:30:56 -0300

    Integrando o Bootstrap na aplicação

--- task-frontend/src/App.js
+++ task-frontend/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Task</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Task</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
