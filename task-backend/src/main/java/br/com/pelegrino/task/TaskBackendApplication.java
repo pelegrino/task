db03267466d8ec6053354fcd465f0451b1e3ba30
Pelegrino <pelegrino@gmail.com>
2023-04-03 10:15:48 -0300

    Criando o projeto no Spring Boot

---
+++ task-backend/src/main/java/br/com/pelegrino/task/TaskBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.pelegrino.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBackendApplication.class, args);
+	}
+
+}
---
+++ task-backend/src/test/java/br/com/pelegrino/task/TaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pelegrino.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
