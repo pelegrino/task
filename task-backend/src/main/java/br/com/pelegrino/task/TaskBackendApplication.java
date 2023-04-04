4d09cc4ac39a629bd6a9843c3bd984be816782c1
Pelegrino <pelegrino@gmail.com>
2023-04-04 09:40:34 -0300

    Expondo o ID das tarefas via web services

--- task-backend/src/main/java/br/com/pelegrino/task/TaskBackendApplication.java
+++ task-backend/src/main/java/br/com/pelegrino/task/TaskBackendApplication.java
@@ -2,12 +2,22 @@ package br.com.pelegrino.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import br.com.pelegrino.task.domain.task.Task;
 
 @SpringBootApplication
-public class TaskBackendApplication {
+public class TaskBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskBackendApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
