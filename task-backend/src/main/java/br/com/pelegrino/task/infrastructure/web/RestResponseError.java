0cf3cba626a35fa5de7bf5e136e93b5f724dd660
Pelegrino <pelegrino@gmail.com>
2023-04-05 10:39:57 -0300

    Mudando a forma como os erros são retornados

---
+++ task-backend/src/main/java/br/com/pelegrino/task/infrastructure/web/RestResponseError.java
@@ -0,0 +1,30 @@
+package br.com.pelegrino.task.infrastructure.web;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+
+public class RestResponseError {
+	
+	private String error;
+
+	public RestResponseError() {
+	}
+	
+	public String getError() {
+		return error;
+	}
+	
+	public static RestResponseError fromValidationErrors(Errors errors) {
+		RestResponseError resp = new RestResponseError();
+		StringBuilder sb = new StringBuilder();
+		
+		for(ObjectError error : errors.getAllErrors()) {
+			sb.append(error.getDefaultMessage()).append(". ");
+			
+		}
+		
+		resp.error = sb.toString();
+		return resp;
+	}
+
+}
---
+++ task-backend/src/main/java/br/com/pelegrino/task/infrastructure/web/WebRequestExceptionHandler.java
@@ -0,0 +1,18 @@
+package br.com.pelegrino.task.infrastructure.web;
+
+import org.springframework.data.rest.core.RepositoryConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class WebRequestExceptionHandler {
+	
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public RestResponseError handleException(RepositoryConstraintViolationException e) {
+		return RestResponseError.fromValidationErrors(e.getErrors());
+	}
+
+}
