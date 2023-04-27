b12774a0970e3ca4ad06a6454611ccec7985c8d2
Pelegrino <pelegrino@gmail.com>
2023-04-27 09:16:39 -0300

    Hook: capurando a movimentação do mouse na tela

---
+++ task-frontend/src/hooktests/useMouseMove.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export const useMouseMove = () => {
+    const [ coords, setCoords ] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        document.addEventListener("mousemove", (e) => {
+            setCoords({ x: e.clientX, y: e.clientY});
+        })
+        return () => document.removeEventListener("mousemove");
+
+    }, []);
+
+    return coords;
+}
--- task-frontend/src/index.js
+++ task-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
-import Title from './hooktests/Title';
+import MouseMove from './hooktests/MouseMove';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Title />, document.getElementById('root'));
+ReactDOM.render(<MouseMove />, document.getElementById('root'));
