df8edbe625739d2bf758fb6ce0558d6b4a407681
Pelegrino <pelegrino@gmail.com>
2023-04-29 09:17:39 -0300

    Compartilhando estados com o useContext

--- task-frontend/src/hooktests/Counter.js
+++ task-frontend/src/hooktests/Counter.js
@@ -1,16 +1,20 @@
 import React from "react"
-import { useCounter } from "./useCounter";
+import { CounterContext } from "./CounterComposed";
 
 const Counter = () => {
 
-    const counter = useCounter();
+    //const counter = useContext(CounterContext);
 
     return (
         <div className="App">
-            <center>
-                <h1>{counter.value}</h1>
-                <input type="button" value="Incrementar" onClick={() => counter.increment()} />
-            </center>
+            <CounterContext.Consumer >
+                { counter => 
+                <center>
+                    <h1>{counter.value}</h1>
+                    <input type="button" value="Incrementar" onClick={() => counter.increment()} />
+                </center>
+                }
+            </CounterContext.Consumer>
         </div>
     );
 }
--- task-frontend/src/index.js
+++ task-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
-import Counter from './hooktests/Counter';
+import CounterComposed from './hooktests/CounterComposed';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<CounterComposed />, document.getElementById('root'));
